9d1f8205e1ccd3f5829756a40a5fbc20621a700d
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-03-04 17:03:15 +0300

    Create navigation menu

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 import LoadAverage from "./LoadAverage";
+import './App.css';
 
 function Home() {
   return <h2>Главная страница</h2>;
@@ -10,27 +12,56 @@ function About() {
   return <h2>О нас</h2>;
 }
 
+function Memory() {
+  return <h2>Память</h2>;
+}
+function IO() {
+  return <h2>I/O</h2>;
+}
+function CPU() {
+  return <h2>CPU</h2>;
+}
+function Disk() {
+  return <h2>Нагрузка на диски</h2>;
+}
+
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/">
+              <FaHome className="icon" />
+            </Link>
           </li>
-          <li>
+
+          <li className="nav-item">
             <Link to="/about">О нас</Link>
           </li>
-            <li>
-                <Link to="/metrics/load-average">Метрики</Link>
-            </li>
+
+          <li className="nav-item">
+            <Link to="/metrics">Метрики</Link>
+            <ul className="dropdown-content">
+              <li><Link to="/metrics/memory">Память</Link></li>
+              <li><Link to="/metrics/io">I/O</Link></li>
+              <li><Link to="/metrics/cpu">CPU</Link></li>
+              <li><Link to="/metrics/disk">Нагрузка на диски</Link></li>
+            </ul>
+          </li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-          <Route path="/metrics/load-average" element={<LoadAverage />} />
+
+        <Route path="/metrics" element={<LoadAverage />} />
+
+        <Route path="/metrics/memory" element={<Memory />} />
+        <Route path="/metrics/io" element={<IO />} />
+        <Route path="/metrics/cpu" element={<CPU />} />
+        <Route path="/metrics/disk" element={<Disk />} />
       </Routes>
     </Router>
   );
